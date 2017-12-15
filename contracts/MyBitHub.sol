04d0c56b04eb68210befcb772743d71c4d51a9c7
Kyle Dewy <dewhurst.kyle@protonmail.ch>
2017-12-15 11:34:47 -0500

    added remove asset method & approval

--- contracts/MyBitHub.sol
+++ contracts/MyBitHub.sol
@@ -9,6 +9,13 @@ import './Asset.sol';
 contract MyBitHub is Pausable {
 using SafeMath for *;
 
+// ------------Approval--------------
+	mapping (address => bool) public userApproved;   // users who are approved to create assets
+
+// -----------Init info--------------------
+uint256 public assetDeposit; 
+
+
 //------------Beneficiary amounts---------------
   uint256 public myBitFoundationPercentage = 1;
   uint256 public lockedTokensPercentage = 2;
@@ -23,25 +30,58 @@ using SafeMath for *;
   // -------------Asset Info------------------------------
 	mapping (address => bytes32) public assetType;		// ID lookup for the title of asset this hub creates....TODO: is this necessary? 
 	mapping (bytes32 => address[]) public assets;  // Address location of assets
+
 	bytes32[] public allAssetTypes;       // All known asset types on the platform
 
+  enum Stages {
+    CreatingAssets,
+    CleaningAssets
+  }
+
+  Stages public stage; 
 
 	function MyBitHub() public {
+
 	}
 
-	// TODO: Stop people from spamming wrong assets?  (Ie. upfront cost + approval process)
+	//  Function which removes old or failed assets. Freezes new assets from being created while the data structure is being updated
+	// TODO: is this at risk of reentrancy
+	function removeAsset(address _assetToBeRemoved, uint256 _assetID)
+	external 
+	whenNotPaused
+	atStage(Stages.CreatingAssets)
+	transitionNext
+	returns (bool) {
+		require(_assetToBeRemoved != address(0)); 
+		require(assets[assetType[_assetToBeRemoved]][_assetID] == _assetToBeRemoved);   // Should catch cases where list is already empty
+		Asset finishedAsset = Asset(_assetToBeRemoved);
+		require(finishedAsset.fundingDeadline() < block.timestamp);    // Make sure Asset has passed funding deadline 
+		require(uint(finishedAsset.stage()) == 1 || msg.sender == owner); 					// Make sure Asset is still in funding stage (Not a success & Not receiving income) unless owner is the sender 
+		uint256 indexToMove = assets[assetType[_assetToBeRemoved]].length - 1; 
+		assets[assetType[_assetToBeRemoved]][_assetID] = assets[assetType[_assetToBeRemoved]][indexToMove];   // Move last address in list to the position being deleted
+		assets[assetType[_assetToBeRemoved]].length--; 		// Reduce list length, deleting the duplicate of indexToMove 
+		delete assetType[_assetToBeRemoved]; 
+		msg.sender.transfer(assetDeposit); 
+		return true;
+	}
+
+	// TODO: Create deposit 
 	function createAsset(bytes32 _storageHash, uint256 _amountToBeRaised, address _assetInstaller, bytes32 _assetType) 
 	whenNotPaused
+	atStage(Stages.CreatingAssets)
+	onlyApproved
+	payable
 	external 
 	returns (address) {
 		require(acceptedAssetType[_assetType]); 
 		require(_storageHash != bytes32(0)); 
 		require(_amountToBeRaised >= 0); 
-		require(_assetInstaller != address(0)); 
+		require(_assetInstaller != address(0));
+		require(msg.value >= assetDeposit);  
 		Asset newAsset = new Asset(msg.sender, _storageHash, _assetInstaller, _amountToBeRaised, fundingTimeForType[_assetType], assets[_assetType].length);
 		assets[_assetType].push(address(newAsset));
 		assetType[address(newAsset)] = _assetType; 
-		assetCreated(msg.sender, _assetInstaller, _assetType, _amountToBeRaised, address(newAsset), assets[_assetType].length);
+		LogAssetCreated(msg.sender, _assetInstaller, _assetType, _amountToBeRaised, address(newAsset), assets[_assetType].length);
 		return address(newAsset);
 	}
 
@@ -56,7 +96,7 @@ using SafeMath for *;
 		acceptedAssetType[_newType] = true;
 		allAssetTypes.push(_newType);
 		fundingTimeForType[_newType] = _timeGivenForFunding;
-		assetTypeAdded(_newType, _timeGivenForFunding, block.timestamp); 
+		LogAssetTypeAdded(_newType, _timeGivenForFunding, block.timestamp); 
 		return true;
 	}
 
@@ -71,6 +111,49 @@ using SafeMath for *;
 		return true; 
 	}
 
+  function nextStage() 
+  internal {
+    if (stage == Stages.CreatingAssets) { 
+        stage = Stages.CleaningAssets;
+      }
+      else { 
+        stage = Stages.CreatingAssets; 
+      }
+  }
+
+  function approveUser(address _newUser)
+  onlyOwner
+  external
+  returns (bool) { 
+    userApproved[_newUser] = true; 
+    return true;
+  }
+
+  function removeUser(address _user)
+  onlyOwner
+  external
+  returns (bool) { 
+  	userApproved[_user] = false; 
+  	return true;
+  }
+
+  // --------------------------------------------Modifiers-----------------------------------------------------
+  modifier atStage(Stages _stage) {
+    require(stage == _stage);
+    _;
+  }
+
+  modifier transitionNext()  {
+  		nextStage(); 
+      _;
+      nextStage();
+  }
+
+  modifier onlyApproved { 
+  	require(userApproved[msg.sender]); 
+  	_; 
+  }
+  
 // -------------------------------------------------------Getters-------------------------------------------------------
 
 	function getAssetsOfType(bytes32 _assetType)
@@ -100,7 +183,7 @@ using SafeMath for *;
 	}
 
 
-	event assetCreated(address _creator, address _installer, bytes32 _hubType, uint256 _amountToBeRaised, address projectAddress, uint256 _index);
-	event assetTypeAdded(bytes32 indexed _newType, uint256 indexed _timeGivenForFunding, uint256 indexed _timestamp);
+	event LogAssetCreated(address _creator, address _installer, bytes32 _hubType, uint256 _amountToBeRaised, address projectAddress, uint256 _index);
+	event LogAssetTypeAdded(bytes32 indexed _newType, uint256 indexed _timeGivenForFunding, uint256 indexed _timestamp);
 
 }
