63d53f709d68c4aab4ffe226dfcd201cf7e4889a
Kyle Dewy <dewhurst.kyle@protonmail.ch>
2017-12-17 18:03:09 -0500

    Reduced storage costs, added notes + events

--- contracts/Asset.sol
+++ contracts/Asset.sol
@@ -4,24 +4,24 @@ import './MyBitHub.sol';
 
 // TODO: what happens when someone suicides Ether into a funding period?
 // TODO: WHen calculating percentages, sometimes Solidity rounds down which may leave some Ether in the contract
-// TODO: When period fails, trigger MYBitHub to delete storage and suicide contract. 
+// TODO: When period fails, MyBitHub should move Asset storage to new position, marking failure
+
+// Created by MyBitHub, holds/distributes Ether for single asset
 contract Asset {
 using SafeMath for *;
 
-  MyBitHub public myBitHub;
+  MyBitHub public myBitHub; 
 
-  // Created by myBit, holds/distributes Ether for Project
   // ------------Project Information--------------
   address public projectCreator;    // Address that initiated the project 
   bytes32 public storageHash;   // Where the title and description + images are stored. (IPFS, Swarm, BigChainDB)
-  uint256 public deadline;        // When the funding period is over 
-  uint256 public creationDate;    // Maybe just index this in a log to save gas? 
+  uint256 public fundingDeadline;        // When the funding period is over 
   uint256 public id;             // Location in MyBitHub assets[type] mapping => list storage
 
-  uint256 public amountToBeRaised;
+  uint256 public amountToBeRaised;  // Amount required for funding to be success 
 
 // -----------Beneficiary Addresses----------------------
-  address public myBitFoundation = address(0);      // mybit foundation address
+  address public myBitFoundation = 0x0089cc346a75b584aae57459d8de170a3af10dff;      // ropsten testnet address
   address public assetInstaller;
 
 
@@ -29,15 +29,15 @@ using SafeMath for *;
   uint256 public amountRaised;          // Total amount funded for project 
   mapping (address => uint256) public shares;     // Portion of the total funded amount held by this individual
   address[] public contributors;    // AssetFunders
-  uint256 public totalPaidToFunders;          // Total amount of Ether redeemed 
   mapping (address => uint256) public paidToFunder;    // Amount investor has withdrawn
 
   // -------Investment Returns--------------
-  uint256 public totalROIReceived;   // Total amount received from Asset
+  uint256 public totalIncomeEarned;   // Total amount received from Asset
+  uint256 public totalPaidToFunders;          // Total amount of Ether redeemed 
 
  // --------Stages & Timing------------
-  Stages public stages;
-  bool private rentrancy_lock = false;
+  Stages public stage;
+  bool private rentrancy_lock = false;    // Prevents re-entrancy attack
 
 
   enum Stages {
@@ -46,7 +46,9 @@ using SafeMath for *;
     ReceivingROI
   }
 
-  // TODO: Test storage on Swarm/BigchainDB/IPFS
+  // @Param: Creator of this asset contract
+  // @Param: The location of this assets information found on IPFS
+  // @Param: The Ether address of the asset installer. TODO: figure out this process 
   function Asset(address _creator, bytes32 _storageHash, address _assetInstaller, uint256 _amountToBeRaised, uint256 _minimumFundingTime, uint256 _id) 
   public {
     myBitHub = MyBitHub(msg.sender);
@@ -54,14 +56,13 @@ using SafeMath for *;
     assetInstaller = _assetInstaller;
     amountToBeRaised = _amountToBeRaised;
     amountRaised = 0;
-    creationDate = block.timestamp;
-    deadline = _minimumFundingTime.add(now);
+    fundingDeadline = _minimumFundingTime.add(now);
     storageHash = _storageHash;
     id = _id;
-    assetCreated(projectCreator, _storageHash, amountToBeRaised, amountRaised, deadline, now);
+    assetCreated(projectCreator, _storageHash, amountToBeRaised, fundingDeadline, now);
   }
 
-  // Users can invest in the asset here
+  // @Dev: Users can send Ether here to fund asset
   // Requires that the funding goal hasn't been reached and that the funding period isn't over. Will move stage to FundingSuccess once goal is reached
   function fund()
   payable 
@@ -80,8 +81,8 @@ using SafeMath for *;
     return true;
   }
 
-  //  TODO: Send installer remaining Ether or predefined percentage? Worried about rounding errors leaving excess Ether
-  // TODO: reduce gas 
+  // This is called once funding has succeeded. Sends Ether to installer, foundation and token holders
+  // TODO: add event
   function payout() 
   atStage(Stages.FundingSuccess) 
   nonReentrant 
@@ -91,29 +92,29 @@ using SafeMath for *;
     uint256 myBitAmount = amountRaised.getFractionalAmount(myBitHub.myBitFoundationPercentage());
     uint256 lockedTokenAmount = amountRaised.getFractionalAmount(myBitHub.lockedTokensPercentage());
     uint256 installerAmount = amountRaised.getFractionalAmount(myBitHub.installerPercentage());
-    assert (myBitAmount != 0 && lockedTokenAmount != 0 && installerAmount != 0);   
     // TODO: send to locked contract
     myBitFoundation.transfer(myBitAmount);
-    assetInstaller.transfer(this.balance);   // send the remainder of Ether
-    stages = Stages.ReceivingROI; 
+    assetInstaller.transfer(this.balance);   // Note: Asset installer will likely receive small amount more, due rounding
+    stage = Stages.ReceivingROI; 
     return true;
   }
 
-
-  function receiveROI(string _note) 
+  // Revenue produced by the asset will be sent here
+  function receiveIncome(string _note) 
   payable 
   requiresEther 
   atStage(Stages.ReceivingROI)
   external 
   returns (bool)  {
-    totalROIReceived = totalROIReceived.add(msg.value); 
-    receivedROI(msg.sender, msg.value, _note, block.timestamp); 
+    totalIncomeEarned = totalIncomeEarned.add(msg.value); 
+    receivedIncome(msg.sender, msg.value, _note, block.timestamp); 
     return true; 
 
   }
 
-  //  contributors can retrieve their funds here if campaign is over + failure.
+  // Contributors can retrieve their funds here if campaign is over + failure.
   // TODO: reduce gas by not storing owed value
+  // TODO: add event 
   function refund() 
   nonReentrant 
   atStage(Stages.FundingAsset) 
@@ -130,7 +131,7 @@ using SafeMath for *;
 
   // TODO: is this method able to be tradeable ....ie. transfer shares when still owed a balance 
   // TODO: the percentage of shares traded, must also transfer the same percentage of paidToFunder
-  function withdraw()
+  function withdrawIncome()
   nonReentrant
   atStage(Stages.ReceivingROI)
   whenNotPaused
@@ -138,25 +139,14 @@ using SafeMath for *;
   returns (bool){
     require(shares[msg.sender] > 0);
     uint256 totalReceived = this.balance.add(totalPaidToFunders);
-    assert (totalROIReceived == totalReceived); 
+    assert (totalIncomeEarned == totalReceived);    // For testing TODO
     uint256 payment = totalReceived.mul(shares[msg.sender]).div(amountRaised).sub(paidToFunder[msg.sender]);
-    assert (payment != 0);
-    assert (this.balance >= payment);
     paidToFunder[msg.sender] = paidToFunder[msg.sender].add(payment);
     totalPaidToFunders = totalPaidToFunders.add(payment);
     msg.sender.transfer(payment);
     investmentRedeemed(msg.sender, payment, block.timestamp); 
     return true;
 }
-  // TODO: Decide who has permission for this
-  function changeAssetInstaller(address _newInstaller) 
-  onlyOwner
-  external
-  returns (bool) { 
-    require(_newInstaller != address(0)); 
-    assetInstaller = _newInstaller; 
-    return true; 
-  }
 
   modifier nonReentrant() {
     require(!rentrancy_lock);
@@ -177,7 +167,7 @@ using SafeMath for *;
   }
 
   modifier atStage(Stages _stage) {
-    require(stages == _stage);
+    require(stage == _stage);
     _;
   }
 
@@ -185,17 +175,17 @@ using SafeMath for *;
     require(amountRaised <= amountToBeRaised);
     _;
     if (amountRaised >= amountToBeRaised) {
-      stages = Stages.FundingSuccess;
+      stage = Stages.FundingSuccess;
       }
   }
 
   modifier withinFundingTime() {
-    require(now <= deadline);
+    require(now <= fundingDeadline);
     _;
   }
 
   modifier fundingPeriodOver() {
-    require(now >= deadline);
+    require(now >= fundingDeadline);
     _;
   }
 
@@ -204,19 +194,19 @@ using SafeMath for *;
     _;
   }
 
-
+  // TODO: seperate into funding info and roi info
   function projectInfo() 
   external 
   view 
   returns (uint256, uint256, uint256, address) {
-    return (amountRaised, amountToBeRaised, deadline, projectCreator);
+    return (amountRaised, amountToBeRaised, fundingDeadline, projectCreator);
   }
 
   function () public {
     revert();
   }
 
-  event assetCreated(address _creator, bytes32 _storageHash, uint256 _amountToBeRaised, uint256 _amountRaised, uint256 _deadline, uint256 _now);
-  event receivedROI(address indexed _sender, uint256 indexed _amount, string _note, uint256 indexed _timestamp); 
+  event assetCreated(address indexed _creator, bytes32 _storageHash, uint256 indexed _amountToBeRaised, uint256 _fundingDeadline, uint256 indexed _now);
+  event receivedIncome(address indexed _sender, uint256 indexed _amount, string _note, uint256 indexed _timestamp); 
   event investmentRedeemed(address indexed _funder, uint256 indexed _amount, uint256 indexed _timestamp);
 }
--- contracts/MyBitHub.sol
+++ contracts/MyBitHub.sol
@@ -5,185 +5,119 @@ import './Pausable.sol';
 import './Asset.sol';
 
 
-// TODO: Find carrying capacity of this contract
+// This contract is in charge of creating individual asset contracts. It acts as a reference for locations of Assets and other funding parameters
+// TODO: Remove broken assets from storage
 contract MyBitHub is Pausable {
 using SafeMath for *;
 
-// ------------Approval--------------
-	mapping (address => bool) public userApproved;   // users who are approved to create assets
-
-// -----------Init info--------------------
-uint256 public assetDeposit; 
-
-
 //------------Beneficiary amounts---------------
-  uint256 public myBitFoundationPercentage = 1;
-  uint256 public lockedTokensPercentage = 2;
-  uint256 public installerPercentage = 97;
+  uint256 public myBitFoundationPercentage = 1;     // Percentage of funding given to MyBit foundation
+  uint256 public lockedTokensPercentage = 2;        // Percentage of funding given to locked token holders
+  uint256 public installerPercentage = 97;          // Percentage of funding given to asset installer 
 
 // ------------------Asset Type Info-------------------
-	// Note: The type of asset is the sha3() of it's string identifier. ie. Solar, ATM, Miner
-	mapping (bytes32 => bool) public acceptedAssetType;      // Is this title accepted on the platform
-	mapping (bytes32 => uint256) public fundingTimeForType;  // time given for funding period for given title of asset
+  // Note: The type of asset is the sha3() of it's string identifier. ie. Solar, ATM, Miner
+  mapping (bytes32 => bool) public acceptedAssetType;      // Tells whether this asset type is accepted on platform. See events for all asset types
+  uint256 public fundingTime;            // The amount of time given for an assets funding period
+
 
 
   // -------------Asset Info------------------------------
-	mapping (address => bytes32) public assetType;		// ID lookup for the title of asset this hub creates....TODO: is this necessary? 
-	mapping (bytes32 => address[]) public assets;  // Address location of assets
+  // NOTE: Could store asset information in log events to save gas 
+  mapping (uint256 => address) public assets;  // Address location of assets. Initialized once funding is success
+  uint256 public assetCounter;          // Counter that keeps track of number of assets
 
-	bytes32[] public allAssetTypes;       // All known asset types on the platform
+  mapping (address => bool) public beingFunded;     // Is this asset currently going through funding stage
 
-  enum Stages {
-    CreatingAssets,
-    CleaningAssets
+  function MyBitHub() public {
+    fundingTime = 300;   // TODO: testing number
   }
 
-  Stages public stage; 
-
-	function MyBitHub() public {
-
-	}
-
-	//  Function which removes old or failed assets. Freezes new assets from being created while the data structure is being updated
-	// TODO: is this at risk of reentrancy
-	function removeAsset(address _assetToBeRemoved, uint256 _assetID)
-	external 
-	whenNotPaused
-	atStage(Stages.CreatingAssets)
-	transitionNext
-	returns (bool) {
-		require(_assetToBeRemoved != address(0)); 
-		require(assets[assetType[_assetToBeRemoved]][_assetID] == _assetToBeRemoved);   // Should catch cases where list is already empty
-		Asset finishedAsset = Asset(_assetToBeRemoved);
-		require(finishedAsset.fundingDeadline() < block.timestamp);    // Make sure Asset has passed funding deadline 
-		require(uint(finishedAsset.stage()) == 1 || msg.sender == owner); 					// Make sure Asset is still in funding stage (Not a success & Not receiving income) unless owner is the sender 
-		uint256 indexToMove = assets[assetType[_assetToBeRemoved]].length - 1; 
-		assets[assetType[_assetToBeRemoved]][_assetID] = assets[assetType[_assetToBeRemoved]][indexToMove];   // Move last address in list to the position being deleted
-		assets[assetType[_assetToBeRemoved]].length--; 		// Reduce list length, deleting the duplicate of indexToMove 
-		delete assetType[_assetToBeRemoved]; 
-		msg.sender.transfer(assetDeposit); 
-		return true;
-	}
-
-	// TODO: Create deposit 
-	function createAsset(bytes32 _storageHash, uint256 _amountToBeRaised, address _assetInstaller, bytes32 _assetType) 
-	whenNotPaused
-	atStage(Stages.CreatingAssets)
-	onlyApproved
-	payable
-	external 
-	returns (address) {
-		require(acceptedAssetType[_assetType]); 
-		require(_storageHash != bytes32(0)); 
-		require(_amountToBeRaised >= 0); 
-		require(_assetInstaller != address(0));
-		require(msg.value >= assetDeposit);  
-		Asset newAsset = new Asset(msg.sender, _storageHash, _assetInstaller, _amountToBeRaised, fundingTimeForType[_assetType], assets[_assetType].length);
-		assets[_assetType].push(address(newAsset));
-		assetType[address(newAsset)] = _assetType; 
-		LogAssetCreated(msg.sender, _assetInstaller, _assetType, _amountToBeRaised, address(newAsset), assets[_assetType].length);
-		return address(newAsset);
-	}
-
-		// Allow a new asset title to be funded on the platform
-	function addAssetType(bytes32 _newType, uint256 _timeGivenForFunding) 
-	onlyOwner 
-	external 
-	returns (bool) {
-		require(!acceptedAssetType[_newType]);
-		require(_timeGivenForFunding >= 0); 
-		require(_newType != bytes32(0)); 
-		acceptedAssetType[_newType] = true;
-		allAssetTypes.push(_newType);
-		fundingTimeForType[_newType] = _timeGivenForFunding;
-		LogAssetTypeAdded(_newType, _timeGivenForFunding, block.timestamp); 
-		return true;
-	}
-
-
-	function changeFundingTimeForAsset(bytes32 _assetType, uint256 _newTimeGivenForFunding) 
-	onlyOwner
-	external
-	returns (bool) { 
-		require(acceptedAssetType[_assetType]); 
-		require(_newTimeGivenForFunding > 0);
-		fundingTimeForType[_assetType] = _newTimeGivenForFunding; 
-		return true; 
-	}
-
-  function nextStage() 
-  internal {
-    if (stage == Stages.CreatingAssets) { 
-        stage = Stages.CleaningAssets;
-      }
-      else { 
-        stage = Stages.CreatingAssets; 
-      }
-  }
-
-  function approveUser(address _newUser)
-  onlyOwner
+  // This is called by the asset contract if it achieves it's funding goal
+  function assetSuccessfullyFunded()
   external
   returns (bool) { 
-    userApproved[_newUser] = true; 
+    require(beingFunded[msg.sender]);
+    delete beingFunded[msg.sender]; 
+    assets[assetCounter] = msg.sender;
+    LogAssetFundingSuccess(msg.sender, assetCounter, block.timestamp); 
+    assetCounter++;
+    return true; 
+  }
+
+  // This is called by the asset contract if it does not achieve it's funding goal
+  function assetFailedFunding(uint256 _amountRaised)
+  external
+  returns (bool) { 
+    require(beingFunded[msg.sender]);
+    delete beingFunded[msg.sender];
+    LogAssetFundingFailed(msg.sender, _amountRaised, block.timestamp); 
     return true;
   }
 
-  function removeUser(address _user)
+  // This money creates an Asset contract to commence funding stage of it's lifecycle. The location is logged in an event. 
+  function createAsset(bytes32 _storageHash, uint256 _amountToBeRaised, address _assetInstaller, bytes32 _assetType) 
+  whenNotPaused
+  onlyApproved
+  external 
+  returns (address) {
+    require(acceptedAssetType[_assetType]); 
+    require(_storageHash != bytes32(0)); 
+    require(_amountToBeRaised >= 0); 
+    require(_assetInstaller != address(0));
+    Asset newAsset = new Asset(msg.sender, _storageHash, _assetInstaller, _amountToBeRaised, fundingTime, _assetType);
+    beingFunded[address(newAsset)] = true; 
+    LogAssetFundingStarted(msg.sender, address(newAsset), _assetType);      // Use indexed event to keep track of pending assets
+    return address(newAsset);
+  }
+
+    // Allow a new asset title to be funded on the platform
+  function addAssetType(bytes32 _newType) 
+  onlyOwner 
+  external 
+  returns (bool) {
+    require(!acceptedAssetType[_newType]);
+    require(_newType != bytes32(0)); 
+    acceptedAssetType[_newType] = true;
+    LogAssetTypeAdded(_newType, block.timestamp); 
+    return true;
+  }
+
+  // Do not let assets of this type be created
+  function removeAssetType(bytes32 _assetType)
   onlyOwner
   external
   returns (bool) { 
-  	userApproved[_user] = false; 
-  	return true;
+    acceptedAssetType[_assetType] = false; 
+    LogAssetTypeRemoved(_assetType, block.timestamp); 
+  }
+
+  // Change the default funding time for the asset 
+  // TODO: Should this be dependent on the asset type?  
+  function changeFundingTime(uint256 _newTimeGivenForFunding) 
+  onlyOwner
+  external
+  returns (bool) { 
+    require(_newTimeGivenForFunding > 0);
+    fundingTime = _newTimeGivenForFunding; 
+    return true; 
   }
 
   // --------------------------------------------Modifiers-----------------------------------------------------
-  modifier atStage(Stages _stage) {
-    require(stage == _stage);
-    _;
-  }
 
-  modifier transitionNext()  {
-  		nextStage(); 
-      _;
-      nextStage();
-  }
 
-  modifier onlyApproved { 
-  	require(userApproved[msg.sender]); 
-  	_; 
-  }
   
 // -------------------------------------------------------Getters-------------------------------------------------------
 
-	function getAssetsOfType(bytes32 _assetType)
-	 view 
-	 external 
-	 returns (address[]) { 
-		return assets[_assetType]; 
-	}
 
-	function getNumAssetsOfType(bytes32 _assetType)
-		view
-		external
-		returns (uint256){ 
-			return assets[_assetType].length; 
-		}
-
-	function getAllAssetTypes()
-	view
-	external
-	returns (bytes32[]) { 
-		return allAssetTypes;
-	}
-	
-
-	function () public {
-		revert();
-	}
+  function () public {
+    revert();
+  }
 
 
-	event LogAssetCreated(address _creator, address _installer, bytes32 _hubType, uint256 _amountToBeRaised, address projectAddress, uint256 _index);
-	event LogAssetTypeAdded(bytes32 indexed _newType, uint256 indexed _timeGivenForFunding, uint256 indexed _timestamp);
-
+  event LogAssetFundingStarted(address indexed _creator, address indexed _assetLocation, bytes32 indexed _assetType);
+  event LogAssetFundingFailed(address indexed _assetLocation, uint256 indexed _amountRaised, uint256 indexed _timestamp); 
+  event LogAssetFundingSuccess(address indexed _assetLocation, uint256 indexed _id, uint256 indexed _timestamp); 
+  event LogAssetTypeAdded(bytes32 indexed _newType, uint256 indexed _timestamp);
+  event LogAssetTypeRemoved(bytes32 indexed _assetType, uint256 indexed _timestamp); 
 }
