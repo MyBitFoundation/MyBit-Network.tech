e4274f691c880d044708922295c4312aa5c0ef39
Kyle Dewy <dewhurst.kyle@protonmail.ch>
2017-12-19 16:21:50 +0000

    removed type storage + added escrow address

--- contracts/MyBitHub.sol
+++ contracts/MyBitHub.sol
@@ -6,9 +6,13 @@ import './Asset.sol';
 
 
 // This contract is in charge of creating individual asset contracts. It acts as a reference for locations of Assets and other funding parameters
-// TODO: Remove broken assets from storage
 contract MyBitHub is Pausable {
 using SafeMath for *;
+
+//-----------Platform Addresses----------------
+address public myBitFoundation;      // ropsten testnet address TODO: Get real foundation address
+address public assetEscrow;     // The location where asset funding is sent to be converted to Fiat
+
 
 //------------Beneficiary amounts---------------
   uint256 public myBitFoundationPercentage = 1;     // Percentage of funding given to MyBit foundation
@@ -17,9 +21,7 @@ using SafeMath for *;
 
 // ------------------Asset Type Info-------------------
   // Note: The type of asset is the sha3() of it's string identifier. ie. Solar, ATM, Miner
-  mapping (bytes32 => bool) public acceptedAssetType;      // Tells whether this asset type is accepted on platform. See events for all asset types
   uint256 public fundingTime;            // The amount of time given for an assets funding period
-
 
 
   // -------------Asset Info------------------------------
@@ -29,8 +31,10 @@ using SafeMath for *;
 
   mapping (address => bool) public beingFunded;     // Is this asset currently going through funding stage
 
-  function MyBitHub() public {
-    fundingTime = 300;   // TODO: testing number
+  function MyBitHub(address _myBitFoundation, address _assetEscrow) public {
+    fundingTime = 300;   // TODO: this is only for testing 
+    myBitFoundation = _myBitFoundation;  
+    assetEscrow = _assetEscrow; 
   }
 
   // This is called by the asset contract if it achieves it's funding goal
@@ -56,55 +60,43 @@ using SafeMath for *;
   }
 
   // This money creates an Asset contract to commence funding stage of it's lifecycle. The location is logged in an event. 
-  function createAsset(bytes32 _storageHash, uint256 _amountToBeRaised, address _assetInstaller, bytes32 _assetType) 
+  function createAsset(bytes32 _storageHash, uint256 _amountToBeRaised, bytes32 _installerID, bytes32 _assetType) 
   whenNotPaused
   onlyApproved
   external 
   returns (address) {
-    require(acceptedAssetType[_assetType]); 
     require(_storageHash != bytes32(0)); 
     require(_amountToBeRaised >= 0); 
-    require(_assetInstaller != address(0));
-    Asset newAsset = new Asset(msg.sender, _storageHash, _assetInstaller, _amountToBeRaised, fundingTime, _assetType);
+    Asset newAsset = new Asset(msg.sender, _storageHash, _installerID, _amountToBeRaised, fundingTime, _assetType);
     beingFunded[address(newAsset)] = true; 
     LogAssetFundingStarted(msg.sender, address(newAsset), _assetType);      // Use indexed event to keep track of pending assets
     return address(newAsset);
   }
 
-    // Allow a new asset title to be funded on the platform
-  function addAssetType(bytes32 _newType) 
-  onlyOwner 
-  external 
-  returns (bool) {
-    require(!acceptedAssetType[_newType]);
-    require(_newType != bytes32(0)); 
-    acceptedAssetType[_newType] = true;
-    LogAssetTypeAdded(_newType, block.timestamp); 
-    return true;
-  }
-
-  // Do not let assets of this type be created
-  function removeAssetType(bytes32 _assetType)
+  // Removes assets that are no longer functioning
+  // TODO: Is it feasible that an asset gets added while this function is half way through running? Should pause if so. 
+  function removeAsset(uint256 _id) 
   onlyOwner
-  external
-  returns (bool) { 
-    acceptedAssetType[_assetType] = false; 
-    LogAssetTypeRemoved(_assetType, block.timestamp); 
+  returns(bool) {
+    require(assets[_id] != address(0));
+    require(assetCounter > 0); 
+    LogAssetRemoved(assets[_id], _id, block.timestamp); 
+    assets[_id] = assets[(assetCounter - 1)];    // Move latest asset to this position
+    assetCounter--; 
+    LogAssetMoved(assets[_id], assetCounter, block.timestamp); 
+    return true; 
   }
 
   // Change the default funding time for the asset 
-  // TODO: Should this be dependent on the asset type?  
   function changeFundingTime(uint256 _newTimeGivenForFunding) 
   onlyOwner
   external
   returns (bool) { 
     require(_newTimeGivenForFunding > 0);
-    fundingTime = _newTimeGivenForFunding; 
+    fundingTime = _newTimeGivenForFunding;
+    LogFundingTimeChanged(_newTimeGivenForFunding, block.timestamp); 
     return true; 
   }
-
-  // --------------------------------------------Modifiers-----------------------------------------------------
-
 
   
 // -------------------------------------------------------Getters-------------------------------------------------------
@@ -118,6 +110,7 @@ using SafeMath for *;
   event LogAssetFundingStarted(address indexed _creator, address indexed _assetLocation, bytes32 indexed _assetType);
   event LogAssetFundingFailed(address indexed _assetLocation, uint256 indexed _amountRaised, uint256 indexed _timestamp); 
   event LogAssetFundingSuccess(address indexed _assetLocation, uint256 indexed _id, uint256 indexed _timestamp); 
-  event LogAssetTypeAdded(bytes32 indexed _newType, uint256 indexed _timestamp);
-  event LogAssetTypeRemoved(bytes32 indexed _assetType, uint256 indexed _timestamp); 
+  event LogAssetRemoved(address indexed _removedAsset, uint256 indexed _id, uint256 indexed _timestamp); 
+  event LogAssetMoved(address indexed _removedAsset, uint256 indexed _id, uint256 indexed _timestamp);
+  event LogFundingTimeChanged(uint256 _newFundingTime, uint256 _timestamp);  
 }
