3f5eec698f312301b17857b06696a82ce905daec
connorblockchain <connor@mybit.io>
2018-02-14 09:52:05 +0100

    Added funding start date Asset.sol, added totalFunders FundingHub.sol, added sell/buy order getters Marketplace.sol

--- Contracts/contracts/AssetCreation.sol
+++ Contracts/contracts/AssetCreation.sol
@@ -1,23 +1,25 @@
+pragma solidity ^0.4.18;
+
 import './Database.sol';
 import './SafeMath.sol';
 
 
-// This contract handles the logic of initiating the funding period for new assets + setting/modifying funding variables. 
-contract AssetCreation { 
+// This contract handles the logic of initiating the funding period for new assets + setting/modifying funding variables.
+contract AssetCreation {
   using SafeMath for *;
 
   Database public database;
   bool private rentrancy_lock = false;    // Prevents re-entrancy attack
 
   function AssetCreation(address _database)
-  public  { 
+  public  {
       database = Database(_database);
   }
 
   //  Must be called before TokenBurn is deployed to avoid someone creating an asset without fundingTime set
   function init()
   external
-  anyOwner { 
+  anyOwner {
       database.setUint(keccak256("myBitFoundationPercentage"), 1);
       database.setUint(keccak256("stakedTokenPercentage"), 2);
       database.setUint(keccak256("installerPercentage"), 97);
@@ -25,25 +27,26 @@ contract AssetCreation {
   }
 
   // This begins the funding period for an asset. If asset is success it will be added to the assets variable here in AssetCreation
-  // @Param: The storage hash created from IPFS. WIll act as the ID for this asset if funding is a success 
-  // @Param: The amount of WEI required for asset to achieve successfull funding 
-  // @Param: The ID of the installer of this asset 
+  // @Param: The storage hash created from IPFS. WIll act as the ID for this asset if funding is a success
+  // @Param: The amount of WEI required for asset to achieve successfull funding
+  // @Param: The ID of the installer of this asset
   // @Param: The type of asset being created. (ie. Sha3("ATM"))
-  function newAsset(bytes32 _storageHash, uint _amountToBeRaised, bytes32 _installerID, bytes32 _assetType) 
-  external 
+  function newAsset(bytes32 _storageHash, uint _amountToBeRaised, bytes32 _installerID, bytes32 _assetType)
+  external
   whenNotPaused
   nonReentrant
   noEmptyBytes(_storageHash)
   noEmptyBytes(_installerID)
   noEmptyBytes(_assetType)
-  notZero(_amountToBeRaised) 
+  notZero(_amountToBeRaised)
   returns (bool){
     require(database.uintStorage(keccak256("userAccess", msg.sender)) >= 2);
     require(database.uintStorage(keccak256("fundingStage", _storageHash)) == 0);   // This ensures the asset isn't currently live or being funded
-    database.setUint(keccak256("amountToBeRaised", _storageHash), _amountToBeRaised); 
+    database.setUint(keccak256("amountToBeRaised", _storageHash), _amountToBeRaised);
     database.setUint(keccak256("fundingDeadline", _storageHash), block.timestamp.add(database.uintStorage(keccak256("fundingTime"))));
-    database.setUint(keccak256("fundingStage", _storageHash), 1); 
-    LogAssetInfo(_storageHash, _installerID, _assetType); 
+    database.setUint(keccak256("fundingStartDate", _storageHash), block.timestamp.add);
+    database.setUint(keccak256("fundingStage", _storageHash), 1);
+    LogAssetInfo(_storageHash, _installerID, _assetType);
     LogAssetFundingStarted(msg.sender, _storageHash, _assetType);      // Use indexed event to keep track of pending assets
     return true;
   }
@@ -51,7 +54,7 @@ contract AssetCreation {
 
   // This removes asset functionality by setting it to an invalid stage. Must leave variables as users may need to withdrawl income.
   // Other owner must have approved the function to be called in Owned.sol (authorizeFunction())
-  function removeAsset(bytes32 _assetID, address _functionSigner) 
+  function removeAsset(bytes32 _assetID, address _functionSigner)
   external
   anyOwner
   noEmptyBytes(_assetID)
@@ -63,20 +66,20 @@ contract AssetCreation {
     require(database.boolStorage(functionHash));
     database.setBool(functionHash, false);
     database.setUint(keccak256("fundingStage", _assetID), 5);   // Asset won't receive income & shares won't be able to be traded.
-    LogAssetRemoved(msg.sender, _assetID, block.timestamp); 
-    return true; 
+    LogAssetRemoved(msg.sender, _assetID, block.timestamp);
+    return true;
   }
 
   // Change the default funding time for new assets
   // Invariants: Only owner[0] can call  |  new funding time is not 0
-  function changeFundingTime(uint _newTimeGivenForFunding) 
+  function changeFundingTime(uint _newTimeGivenForFunding)
   external
   anyOwner
   notZero(_newTimeGivenForFunding)
-  returns (bool) { 
-    database.setUint(keccak256("fundingTime"), _newTimeGivenForFunding); 
-    LogFundingTimeChanged(msg.sender, _newTimeGivenForFunding, block.timestamp); 
-    return true; 
+  returns (bool) {
+    database.setUint(keccak256("fundingTime"), _newTimeGivenForFunding);
+    LogFundingTimeChanged(msg.sender, _newTimeGivenForFunding, block.timestamp);
+    return true;
   }
 
   // Changes the platform fees received from funded assets
@@ -86,7 +89,7 @@ contract AssetCreation {
   anyOwner
   notZero(_myBitFoundationPercentage)
   notZero(_stakedTokenPercentage)
-  notZero(_installerPercentage) 
+  notZero(_installerPercentage)
   returns (bool) {
     bytes32 functionHash = keccak256(this, _functionSigner, "changeFundingPercentages", keccak256(_myBitFoundationPercentage, _stakedTokenPercentage, _installerPercentage));
     require(database.boolStorage(functionHash));
@@ -98,27 +101,27 @@ contract AssetCreation {
     return true;
   }
 
-  modifier whenNotPaused { 
+  modifier whenNotPaused {
     require(!database.boolStorage(keccak256("pause", this)));
-    _; 
+    _;
   }
 
-  modifier noEmptyAddress(address _addr) { 
-    require(_addr != address(0)); 
-    _; 
+  modifier noEmptyAddress(address _addr) {
+    require(_addr != address(0));
+    _;
   }
 
   modifier noEmptyBytes(bytes32 _data) {
-    require(_data != bytes32(0)); 
+    require(_data != bytes32(0));
     _;
   }
 
-  modifier notZero(uint _uint) { 
-    require(_uint != 0); 
+  modifier notZero(uint _uint) {
+    require(_uint != 0);
     _;
   }
 
-  modifier anyOwner { 
+  modifier anyOwner {
     require(database.boolStorage(keccak256("owner", msg.sender)));
     _;
   }
@@ -131,7 +134,7 @@ contract AssetCreation {
   }
 
   event LogAssetFundingStarted(address indexed _creator, bytes32 indexed _assetLocation, bytes32 indexed _assetType);
-  event LogAssetInfo(bytes32 indexed _storageHash, bytes32 indexed _installerID, bytes32 indexed _assetType); 
-  event LogAssetRemoved(address indexed _remover, bytes32 indexed _id, uint indexed _timestamp); 
+  event LogAssetInfo(bytes32 indexed _storageHash, bytes32 indexed _installerID, bytes32 indexed _assetType);
+  event LogAssetRemoved(address indexed _remover, bytes32 indexed _id, uint indexed _timestamp);
   event LogFundingTimeChanged(address _sender, uint _newTimeForFunding, uint _blockTimestamp);
 }
