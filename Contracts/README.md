0aee36b5ac9bb5b799d197566b1cc42328ab2608
Kyle Dewy <dewhurst.kyle@protonmail.ch>
2018-03-09 12:17:03 +0100

    TokenFaucet added

--- Contracts/README.md
+++ Contracts/README.md
@@ -1,9 +1,24 @@
+
+# Usage 
+> Any user must have the appropriate access level for different contracts/functions. 
+
+> First go to the MyBit Faucet and get at least 10,000 MYB tokens
+
+> To interact with the dapp at all you must have an access level greater or equal to 1. To achieve this you must have KYC approved (any owner can authorize this by calling approveUser() in the Owned.sol contract)
+
+> Once approved you can achieve a higher access level you must burn MyBit tokens in TokenBurn.sol. (2 = Creating & FundingAssets, 3 = Ability to stake, 4 = Trade asset shares on marketplace )
+
+> Before burning tokens you must approve TokenBurn.sol to transfer the required amount of tokens from your account (see TokenBurn.sol for rates)
+
+> Once the tokens are burnt you have access to the platform. 
+
 # Deploying 
 > Set 3 owners when deploying Database. Any one of these owners will be authorized to deploy the rest of the contracts. All contracts that are deployed must be added to the database through ContractManager.
 
 > Test.sol is used to verify that WEI has been sent to the proper address 
 
 > HashFunctions.sol is used as a helper for hash functions
+
 ## Order of deployment:
 
 * MyBitToken(For Testing...if deployed need the address)
---
+++ Contracts/contracts/TokenFaucet.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.4.19;
+
+// ERC20 token implementation. Modified for MyBit transfer() function not returning bool.
+contract Token {
+
+  function totalSupply() constant returns (uint256 supply) {}
+
+  function balanceOf(address _owner) constant returns (uint256 balance) {}
+
+  function transfer(address _to, uint256 _value) {}
+
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+
+  function approve(address _spender, uint256 _value) returns (bool success) {}
+
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+  uint public decimals;
+  string public name;
+}
+
+// Can use this contract to give tokens away. 
+// Note: This is not secure and has no limits placed. Use for test-net only.
+contract TokenFaucet { 
+
+  Token public token;
+  uint public tokensInFaucet; 
+
+  function TokenFaucet(address _tokenAddress)
+  public  { 
+    token = Token(_tokenAddress); 
+  }
+
+  function deposit(uint _amount)
+  external { 
+    require(token.transferFrom(msg.sender, this, _amount)); 
+    tokensInFaucet += _amount;
+    TokenDeposit(msg.sender, _amount, block.number); 
+  }
+
+  function withdraw(uint _amount)
+  external { 
+    require (tokensInFaucet >= _amount); 
+    tokensInFaucet -= _amount;
+    token.transfer(msg.sender, _amount);
+    TokenWithdraw(msg.sender, _amount, block.number);
+  }
+
+  event TokenWithdraw(address _sender, uint _amount, uint _blockNumber); 
+  event TokenDeposit(address _depositer, uint _amount, uint _blockNumber); 
+}
