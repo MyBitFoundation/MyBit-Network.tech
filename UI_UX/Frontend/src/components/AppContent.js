f68a5181914f94bd703ad87a94069df8c32bbbe8
connorblockchain <connor@mybit.io>
2018-03-16 16:04:35 +0100

    framework for UI calls added, not functional RN

--- UI_UX/Frontend/src/components/AppContent.js
+++ UI_UX/Frontend/src/components/AppContent.js
@@ -9,6 +9,13 @@ import {
   Table
 } from 'semantic-ui-react';
 import { getWeb3Async } from '../util/web3';
+import { keccak256 } from 'js-sha3';
+
+/* Smart Contract Utils not Apps */
+import DatabaseUtil from './contracts/DatabaseUtil';
+import FundingHubUtil from './contracts/FundingHubUtil';
+
+/* Images  */
 import Accounts from './Accounts';
 import asset from '../images/bitcoin-atm.png';
 import metamaskAccount from '../images/metamask-account.png';
@@ -18,17 +25,30 @@ export default class AppContent extends React.Component {
     super(props);
     this.state = {
       web3: null,
-      isWeb3synced: false
+      isWeb3synced: false,
+      assetID: null
     };
   }
   async componentDidMount() {
     const web3 = await getWeb3Async();
     if (web3.isConnected()) {
-      this.setState({ web3: web3, isWeb3synced: true });
+      const databaseInstance = new DatabaseUtil();
+      const fundingHubInstance = new FundingHubUtil();
+      databaseInstance.load(web3);
+      fundingHubInstance.load(web3);
+      this.setState({
+        web3: web3,
+        isWeb3synced: true,
+        assetID: 6374856,
+        databaseInstance: databaseInstance,
+        fundingHubInstance: fundingHubInstance
+      });
+      // Fixed assetID for testing
+      // Currently not a real assetID
     }
   }
   render() {
-    const { web3, isWeb3synced } = this.state;
+    const { web3, isWeb3synced, databaseInstance, assetID } = this.state;
     return (
       <Grid>
         <Grid.Row>
@@ -45,7 +65,8 @@ export default class AppContent extends React.Component {
                 marginTop: '5px'
               }}
             >
-              Contribute
+              Contribute{' '}
+              {/* fundingHubInstance.fund(assetID, grabValueFromField)*/}
             </Button>
           </Grid.Column>
           <Grid.Column width={12}>
@@ -76,7 +97,7 @@ export default class AppContent extends React.Component {
                           <Grid.Column width={4}>
                             <Segment compact>
                               <Image src={metamaskAccount} />
-                              {isWeb3synced && <Accounts web3={web3} />}
+                              {isWeb3synced && web3.eth.coinbase}
                             </Segment>
                           </Grid.Column>
                         </Grid.Row>
@@ -92,13 +113,19 @@ export default class AppContent extends React.Component {
                         <Grid.Row className="stretched collapsed">
                           <Grid.Column width={10}>Asset Cost</Grid.Column>
                           <Grid.Column width={6}>
-                            <b>20 ETH</b>
+                            <b>
+                              {isWeb3synced &&
+                                databaseInstance.uintStorage(
+                                  keccak256('amountRaised', assetID)
+                                )}
+                            </b>
                           </Grid.Column>
                         </Grid.Row>
                         <Grid.Row className="stretched collapsed">
                           <Grid.Column width={10}>Maintenance Fee</Grid.Column>
                           <Grid.Column width={6}>
-                            <b>0.1 ETH/m</b>
+                            <b>0.1 ETH/m</b>{' '}
+                            {/* TODO; need to grab from bigchaindb */}
                           </Grid.Column>
                         </Grid.Row>
                         <Grid.Row>
@@ -109,7 +136,8 @@ export default class AppContent extends React.Component {
                         <Grid.Row className="stretched collapsed">
                           <Grid.Column width={10}>Breakeven</Grid.Column>
                           <Grid.Column width={6}>
-                            <b>5 Months</b>
+                            <b>5 Months</b>{' '}
+                            {/* TODO; need to grab from bigchaindb */}
                           </Grid.Column>
                         </Grid.Row>
                         <Grid.Row className="stretched collapsed">
@@ -117,7 +145,8 @@ export default class AppContent extends React.Component {
                             Return of investment
                           </Grid.Column>
                           <Grid.Column width={6}>
-                            <b>18%/Year</b>
+                            <b>18%/Year</b>{' '}
+                            {/* TODO; need to grab from bigchaindb */}
                           </Grid.Column>
                         </Grid.Row>
                         <Grid.Row className="stretched collapsed">
@@ -125,7 +154,8 @@ export default class AppContent extends React.Component {
                             Total Asset Revenue
                           </Grid.Column>
                           <Grid.Column width={6}>
-                            <b>2.3 ETH/m</b>
+                            <b>2.3 ETH/m</b>{' '}
+                            {/* TODO; need to grab from bigchaindb */}
                           </Grid.Column>
                         </Grid.Row>
                         <Grid.Row>
@@ -136,13 +166,19 @@ export default class AppContent extends React.Component {
                         <Grid.Row className="stretched collapsed">
                           <Grid.Column width={10}>Asset ID</Grid.Column>
                           <Grid.Column width={6}>
-                            <b>6374856</b>
+                            <b>6374856</b>{' '}
+                            {/* TODO; need to grab from bigchaindb */}
                           </Grid.Column>
                         </Grid.Row>
                         <Grid.Row className="stretched collapsed">
                           <Grid.Column width={10}>Installer</Grid.Column>
                           <Grid.Column width={6}>
-                            <b>Bitcoin ATM Installer</b>
+                            <b>
+                              {isWeb3synced &&
+                                databaseInstance.bytesStorage(
+                                  keccak256('installerID', assetID)
+                                )}
+                            </b>
                           </Grid.Column>
                         </Grid.Row>
                       </Grid>
@@ -156,27 +192,56 @@ export default class AppContent extends React.Component {
                     <Segment.Group horizontal>
                       <Segment>
                         {' '}
-                        Current funding: <b>0</b>
+                        Current funding:
+                        <b>
+                          {isWeb3synced &&
+                            databaseInstance.uintStorage(
+                              keccak256('amountRaised', assetID)
+                            )}
+                        </b>
                       </Segment>
                       <Segment>
                         {' '}
-                        Funding period ends on Mon, March 26 2018 4:20 PM{' '}
+                        Funding period ends on
+                        {isWeb3synced &&
+                          new Date(
+                            databaseInstance.uintStorage(
+                              keccak256('fundingDeadline', assetID)
+                            )
+                          )}{' '}
                       </Segment>
                       <Segment>
                         {' '}
-                        Funding Goal: <b>20 ETH</b>
+                        Funding Goal:{' '}
+                        <b>
+                          {isWeb3synced &&
+                            databaseInstance.uintStorage(
+                              keccak256('amountToBeRaised', assetID)
+                            )}
+                        </b>
                       </Segment>
                     </Segment.Group>
                     <Segment>
                       <Progress
                         style={{ margin: '0' }}
                         progress
-                        percent={54.5}
+                        percent={
+                          isWeb3synced &&
+                          databaseInstance.uintStorage(
+                            keccak256('amountRaised', assetID)
+                          ) /
+                            databaseInstance.uintStorage(
+                              keccak256('amountToBeRaised', assetID)
+                            ) *
+                            100
+                        }
                         color="blue"
                       />
                     </Segment>
                     <Segment textAlign="right">
-                      Total Contributors: <b>46</b>
+                      Total Contributors:
+                      <b>50</b>
+                      {/* TODO; need to grab from bigchaindb */}
                     </Segment>
                   </Segment.Group>
                 </Grid.Column>
---
+++ UI_UX/Frontend/src/components/contracts/FundingHubUtil.js
@@ -0,0 +1,105 @@
+import { promisifyAll } from 'bluebird';
+
+import ABIInterfaceArray from '../../util/abi/FundingHub.json';
+
+const SMART_CONTRACT_ADDRESS = '0x8e88e493162a6435adfc6809ab713a9cd81e9a1c';
+const instancePromisifier = instance =>
+  promisifyAll(instance, { suffix: 'Async' });
+
+export default class FundingHubUtil {
+  async load(web3) {
+    const abi = await web3.eth.contract(ABIInterfaceArray);
+    this.instance = instancePromisifier(abi.at(SMART_CONTRACT_ADDRESS));
+    this.web3 = web3;
+  }
+
+  async fund(_assetID, _value) {
+    const response = await this.instance.fundAsync(_assetID, {
+      from: this.web3.eth.coinbase,
+      gas: 2000000,
+      value: _value
+    });
+    /*      if(this.modifier.fundingLimitValue(_assetID) &&
+         this.modifier.fundingLimitTime(_assetID) &&
+         this.modifier.onlyApproved(2) &&
+         this.modifier.atStage(_assetID, 1) &&
+         this.modifier.notZero(_value)
+      ){
+        this.instance.fund.estimateGas(
+          _assetID,
+          {from: this.web3.eth.coinbase, value:_value},
+          async function(e,gasEstimate){
+            if(!e){
+              const response = await this.instance.fundAsync(_assetID,{
+                  from: this.web3.eth.coinbase, gas:gasEstimate, value: _value});
+                }
+              })
+            }
+          };*/
+  }
+
+  async payout(_assetID) {
+    const response = await this.instance.payoutAsync(_assetID, {
+      from: this.web3.eth.coinbase,
+      gas: 2000000
+    });
+    /*  if(this.modifier.atStage(_assetID, 3) &&
+         this.modifier.fundingPeriodOver(_assetID)
+      ){
+        this.instance.payout.estimateGas(
+          _assetID,
+          {from: this.web3.eth.coinbase},
+          async function(e,gasEstimate){
+            if(!e){
+              const response = await this.instance.payoutAsync(_assetID,{
+                from: this.web3.eth.coinbase, gas:gasEstimate});
+              }
+          }
+        )
+      }
+    }*/
+  }
+
+  async initiateRefund(_assetID) {
+    const response = await this.instance.initiateRefundAsync(_assetID, {
+      from: this.web3.eth.coinbase,
+      gas: 2000000
+    });
+    /*      if(this.modifier.fundingPeriodOver(_assetID) &&
+         this.modifier.atStage(_assetID, 1)
+      ){
+        this.instance.initiateRefund.estimateGas(
+          _assetID,
+          {from: this.web3.eth.coinbase},
+          async function(e,gasEstimate){
+            if(!e){
+              const response = await this.instance.initiateRefundAsync(_assetID,{
+                from: this.web3.eth.coinbase, gas:gasEstimate});
+              }
+          }
+        )
+      }
+    }*/
+  }
+
+  async refund(_assetID) {
+    const response = await this.instance.refundAsync(_assetID, {
+      from: this.web3.eth.coinbase,
+      gas: 2000000
+    });
+    /*if(this.modifier.atStage(_assetID, 1))
+        {
+          this.instance.refund.estimateGas(
+            _assetID,
+            {from:this.web3.eth.coinbase},
+            async function(e,gasEstimate){
+              if(!e){
+                const response = await this.instance.refundAsync(_assetID,{
+                from: this.web3.eth.coinbase, gas:gasEstimate});
+              }
+            }
+          )
+        }
+      }*/
+  }
+}
