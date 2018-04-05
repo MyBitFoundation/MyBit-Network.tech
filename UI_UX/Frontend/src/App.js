a40e0583fe50bf94b6f0afa066a1695e7017bfa3
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2018-04-05 20:17:35 +0100

    Setup components and stories for all required components in alpha v0.1

--- UI_UX/Frontend/src/App.js
+++ UI_UX/Frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Header } from 'semantic-ui-react';
-import './App.css';
+import './styles/App.css';
 
 import { AppHeader } from './components/AppHeader';
 import { AppSidebar } from './components/AppSidebar';
--- UI_UX/Frontend/src/stories/index.js
+++ UI_UX/Frontend/src/stories/index.js
@@ -4,7 +4,26 @@ import { storiesOf } from '@storybook/react';
 
 import { AppHeader } from '../components/AppHeader';
 import { AppSidebar } from '../components/AppSidebar';
+import { NavigationOption } from '../components/NavigationOption';
+import { Address } from '../components/Address';
+import { ExplorePage } from '../components/ExplorePage';
+import { Category } from '../components/Category';
+import { ExploreAssetsPage } from '../components/ExploreAssetsPage';
+import { FiltersBar } from '../components/FiltersBar';
+import { Button } from '../components/Button';
+import { Asset } from '../components/Asset';
+import { PortfolioPage } from '../components/PortfolioPage';
+import { SmallInfoPanel } from '../components/SmallInfoPanel';
+import { TransactionsPage } from '../components/TransactionsPage';
+import { TransactionHistory } from '../components/TransactionHistory';
+import { Row } from '../components/Row';
+import { AssetDetailsPage } from '../components/AssetDetailsPage';
+import { AssetHero } from '../components/AssetHero';
+import { AssetDetails } from '../components/AssetDetails';
+import { AssetFunding } from '../components/AssetFunding';
+import { ConfirmationPopup } from '../components/ConfirmationPopup';
 import { Grid } from 'semantic-ui-react';
+
 import 'semantic-ui-css/semantic.css';
 
 storiesOf('Sidebar', module).add('view', () => {
@@ -18,3 +37,43 @@ storiesOf('Sidebar', module).add('view', () => {
 });
 
 storiesOf('Header', module).add('view', () => <AppHeader />);
+
+storiesOf('Explore Page', module).add('view', () => <ExplorePage />);
+
+storiesOf('Explore Assets Page', module).add('view', () => (
+  <ExploreAssetsPage />
+));
+
+storiesOf('Portfolio Page', module).add('view', () => <PortfolioPage />);
+
+storiesOf('Transactions Page', module).add('view', () => <TransactionsPage />);
+
+storiesOf('Asset Details Page', module).add('view', () => <AssetDetailsPage />);
+
+storiesOf('Address', module).add('view', () => <Address />);
+
+storiesOf('Category', module).add('view', () => <Category />);
+
+storiesOf('Button', module).add('view', () => <Button />);
+
+storiesOf('Asset', module).add('view', () => <Asset />);
+
+storiesOf('Small Info Panel', module).add('view', () => <SmallInfoPanel />);
+
+storiesOf('Asset Hero', module).add('view', () => <AssetHero />);
+
+storiesOf('Asset Details', module).add('view', () => <AssetDetails />);
+
+storiesOf('Asset Funding', module).add('view', () => <AssetFunding />);
+
+storiesOf('Confirmation Popup', module).add('view', () => (
+  <ConfirmationPopup />
+));
+
+storiesOf('Transaction History', module).add('view', () => (
+  <TransactionHistory />
+));
+
+storiesOf('Row', module).add('view', () => <Row />);
+
+storiesOf('NavigationOption', module).add('view', () => <NavigationOption />);
