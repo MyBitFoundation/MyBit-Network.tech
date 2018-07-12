fa724e2134faf4375bc3cfe15e8ce5952002bf4a
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2018-07-12 11:18:40 -0500

    Fixed all tests to be able to be run locally w/truffle

--- Contracts/truffle.js
+++ Contracts/truffle.js
@@ -1,4 +1,12 @@
 module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 7545,
+      gas: 6500000,
+      network_id: "5777"
+    }
+  },
   compiler: {
      solc: "0.4.23"
   }
