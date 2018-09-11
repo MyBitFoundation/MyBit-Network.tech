9a7c597d5d9fbfa9e67bf0b6ab3b218d24321982
Kyle Dewy <dewhurst.kyle@protonmail.ch>
2018-09-11 18:03:00 +0200

    fixed equal distribution test

--- contracts/ecosystem/BrokerEscrow.sol
+++ contracts/ecosystem/BrokerEscrow.sol
@@ -14,24 +14,38 @@
       database = Database(_database); 
     } 
 
-
+    // @notice broker can deposit tokens into escrow here. 
     function depositEscrow(uint _amount, address _tokenAddress)
     external { 
 
     }
 
-
+    // @dev assetID can be computed beforehand with sha3(msg.sender, _amountToRaise, _operatorID, _assetURI))
     function lockEscrow(uint _amount, bytes32 _assetID)
     external { 
 
     }
 
 
+    // @notice asset must have fundingDeadline = 0 or have ROI > 25%
+    // @dev returns escrow according to ROI. 25% ROI returns 25% of escrow, 50% ROI returns 50% of escrow etc... 
     function unlockEscrow(bytes32 _assetID)
     external { 
 
     }
 
+    // @notice investors can vote to call this function and burn the brokers escrow for negligence
+    function burnEscrow(bytes32 _assetID)
+    external 
+    onlyOwner { 
 
+    }
+
+
+    // @notice Sender must be a registered owner
+    modifier onlyOwner {
+      require(database.boolStorage(keccak256(abi.encodePacked("owner", msg.sender))));
+      _;
+    }
 
 }
