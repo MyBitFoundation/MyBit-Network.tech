07ee3964a92f92a0bb7ddd96704b5fe4a45fa889
Iggy Grey <github@pmphillips.ca>
2018-10-10 20:58:31 -0700

    Updated migration to include api

--- migrations/2_deploy_protocol.js
+++ migrations/2_deploy_protocol.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var BurnableToken = artifacts.require("./tokens/erc20/BurnableToken.sol");
 var Database = artifacts.require("./database/Database.sol");
 var ContractManager = artifacts.require("./database/ContractManager.sol");
+var API = artifacts.require("./database/API.sol");
 var SingleOwned = artifacts.require("./ownership/SingleOwned.sol");
 var Pausible = artifacts.require("./ownership/Pausible.sol");
 var ERC20Burner = artifacts.require("./ecosystem/ERC20Burner.sol");
@@ -21,7 +22,7 @@ var decimals = 1000000000000000000;
 var tokenSupply = 100000*decimals;
 var tokenPerAccount = 100*decimals;
 
-var safemath, MyB, db, cm, owned, pausible, burner, access,
+var safemath, MyB, db, cm, api, owned, pausible, burner, access,
     platform, operators, escrow, crowdsaleETH, crowdsaleGeneratorETH,
     crowdsaleERC20, crowdsaleGeneratorERC20, dax;
 
@@ -34,6 +35,7 @@ module.exports = function(deployer, network, accounts) {
 
     //Link safemath library
     deployer.link(SafeMath,
+                  API,
                   BurnableToken,
                   BrokerEscrow,
                   Operators,
@@ -69,6 +71,14 @@ module.exports = function(deployer, network, accounts) {
     console.log('ContractManager.sol: ' + cm.address);
     db.enableContractManagement(cm.address);
     cm.addContract('Owner', accounts[0]); //Give acounts[0] ability to write to database
+
+    return API.new(db.address);
+
+  }).then(function(instance) {
+
+    api = instance;
+    console.log('API.sol: ' + api.address);
+    cm.addContract('API', api.address);
 
     return SingleOwned.new(db.address);
 
@@ -197,6 +207,7 @@ module.exports = function(deployer, network, accounts) {
       "ERC20Burner" : burner.address,
       "Database" : db.address,
       "ContractManager" : cm.address,
+      "API" : api.address,
       "SingleOwned" : owned.address,
       "Pausible" : pausible.address,
       "AccessHierarchy" : access.address,
