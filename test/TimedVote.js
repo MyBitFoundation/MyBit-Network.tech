13fe9c1d3a107f90992b030760a5397f26faf138
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-10-30 15:22:34 -0400

    vote: Group tests

--- test/TimedVote.js
+++ test/TimedVote.js
@@ -30,132 +30,156 @@ function throws (executor) {
 
 
 contract('TimedVote', () => {
-  describe('~isNullAddress', () => {
-    it('Null', async() => {
-      const nullAddress = await timedVote._isNullAddress(NULL_ADDRESS);
-      assert.isTrue(nullAddress);
-    });
+  // --------------
+  // Pure Condition
+  // --------------
 
-    it('Nonnull', async() => {
-      const nullAddress = await timedVote._isNullAddress(validAddress);
-      assert.isFalse(nullAddress);
-    });
-  });
+  describe('Pure Condition', () => {
+    describe('~isNullAddress', () => {
+      it('Null', async() => {
+        const nullAddress = await timedVote._isNullAddress(NULL_ADDRESS);
+        assert.isTrue(nullAddress);
+      });
 
-  describe('#accountCommitted', () => {
-    it('Uncommitted', async() => {
-      const committed = await timedVote.accountCommitted(user1);
-      assert.isFalse(committed);
-    });
-
-    it('Committed', async() => {
-      await timedVote._setCommitment(user1, 5);
-      const committed = await timedVote.accountCommitted(user1);
-      assert.isTrue(committed);
+      it('Nonnull', async() => {
+        const nullAddress = await timedVote._isNullAddress(validAddress);
+        assert.isFalse(nullAddress);
+      });
     });
   });
 
-  describe('~onlyCommitted', () => {
-    it('Reject uncommitted', throws(async() => {
-      await timedVote._onlyCommitted();
-    }));
+  // --------------
+  // View Condition
+  // --------------
 
-    it('Accept committed', async() => {
-      await timedVote._setCommitment(user1, 5);
-      await timedVote._onlyCommitted();
+  describe('View Condition', () => {
+    describe('#accountCommitted', () => {
+      it('Uncommitted', async() => {
+        const committed = await timedVote.accountCommitted(user1);
+        assert.isFalse(committed);
+      });
+
+      it('Committed', async() => {
+        await timedVote._setCommitment(user1, 5);
+        const committed = await timedVote.accountCommitted(user1);
+        assert.isTrue(committed);
+      });
     });
   });
 
-  describe('~onlyUncommitted', () => {
-    it('Accept uncommitted', async() => {
-      await timedVote._onlyUncommitted();
-    });
+  // --------
+  // Modifier
+  // --------
 
-    it('Reject committed', throws(async() => {
-      await timedVote._setCommitment(user1, 5);
-      await timedVote._onlyUncommitted();
-    }));
-  });
+  describe('Modifier', () => {
+    describe('~onlyCommitted', () => {
+      it('Reject uncommitted', throws(async() => {
+        await timedVote._onlyCommitted();
+      }));
 
-  describe('#commit', () => {
-    it('Fail with value 0', throws(async() => {
-      await timedVote.commit(0);
-    }));
-
-    it('Fail without approval', throws(async() => {
-      await timedVote.commit(5);
-    }));
-
-    it('Fail with insufficient approval', async() => {
-      await token.transfer(user1, 100);
-      await token.approve(timedVote.address, 5, {from: user1});
-      await throws(async() => {
-        await timedVote.commit(100);
+      it('Accept committed', async() => {
+        await timedVote._setCommitment(user1, 5);
+        await timedVote._onlyCommitted();
       });
     });
 
-    it('Succeed', async() => {
-      await token.transfer(user1, 100);
-      await token.approve(timedVote.address, 100, {from: user1});
-      await timedVote.commit(100);
-    });
+    describe('~onlyUncommitted', () => {
+      it('Accept uncommitted', async() => {
+        await timedVote._onlyUncommitted();
+      });
 
-    it('Fail double commit', async() => {
-      await token.transfer(user1, 200);
-      await token.approve(timedVote.address, 200, {from: user1});
-      await timedVote.commit(100);
-      await throws(async() => {
+      it('Reject committed', throws(async() => {
+        await timedVote._setCommitment(user1, 5);
+        await timedVote._onlyUncommitted();
+      }));
+    });
+  });
+
+  // ---------
+  // Interface
+  // ---------
+
+  describe('Interface', () => {
+    describe('#commit', () => {
+      it('Fail with value 0', throws(async() => {
+        await timedVote.commit(0);
+      }));
+
+      it('Fail without approval', throws(async() => {
+        await timedVote.commit(5);
+      }));
+
+      it('Fail with insufficient approval', async() => {
+        await token.transfer(user1, 100);
+        await token.approve(timedVote.address, 5, {from: user1});
+        await throws(async() => {
+          await timedVote.commit(100);
+        });
+      });
+
+      it('Succeed', async() => {
+        await token.transfer(user1, 100);
+        await token.approve(timedVote.address, 100, {from: user1});
         await timedVote.commit(100);
+      });
+
+      it('Fail double commit', async() => {
+        await token.transfer(user1, 200);
+        await token.approve(timedVote.address, 200, {from: user1});
+        await timedVote.commit(100);
+        await throws(async() => {
+          await timedVote.commit(100);
+        });
+      });
+
+      it('Emit Commit', async() => {
+        await token.transfer(user1, 100);
+        await token.approve(timedVote.address, 100, {from: user1});
+        const { logs: events } = await timedVote.commit(100);
+        assert.isAtLeast(events.length, 1);
+        const event = events.pop();
+        assert.strictEqual(event.event, 'Commit');
+        assert.strictEqual(event.args.account, user1);
+        assert.isTrue(BigNumber(event.args.value).isEqualTo(100));
       });
     });
 
-    it('Emit Commit', async() => {
-      await token.transfer(user1, 100);
-      await token.approve(timedVote.address, 100, {from: user1});
-      const { logs: events } = await timedVote.commit(100);
-      assert.isAtLeast(events.length, 1);
-      const event = events.pop();
-      assert.strictEqual(event.event, 'Commit');
-      assert.strictEqual(event.args.account, user1);
-      assert.isTrue(BigNumber(event.args.value).isEqualTo(100));
-    });
-  });
+    describe('#commitment', () => {
+      it('Find 0 commitment', async() => {
+        const result = await timedVote.commitment.call(user1);
+        assert.isTrue(BigNumber(result).isEqualTo(0));
+      });
 
-  describe('#commitment', () => {
-    it('Find 0 commitment', async() => {
-      const result = await timedVote.commitment.call(user1);
-      assert.isTrue(BigNumber(result).isEqualTo(0));
+      it('Find nonzero commitment', async() => {
+        await timedVote._setCommitment(user1, 100);
+        const result = await timedVote.commitment.call(user1);
+        assert.isTrue(BigNumber(result).isEqualTo(100));
+      });
     });
 
-    it('Find nonzero commitment', async() => {
-      await timedVote._setCommitment(user1, 100);
-      const result = await timedVote.commitment.call(user1);
-      assert.isTrue(BigNumber(result).isEqualTo(100));
-    });
-  });
+    describe('#withdraw', () => {
+      it('Fail without commitment', throws(async() => {
+        await timedVote.withdraw();
+      }));
 
-  describe('#withdraw', () => {
-    it('Fail without commitment', throws(async() => {
-      await timedVote.withdraw();
-    }));
+      it('Succeed', async() => {
+        await token.transfer(user1, 100);
+        await token.approve(timedVote.address, 100, {from: user1});
+        await timedVote.commit(100);
+        await timedVote.withdraw();
+      });
 
-    it('Succeed', async() => {
-      await token.transfer(user1, 100);
-      await token.approve(timedVote.address, 100, {from: user1});
-      await timedVote.commit(100);
-      await timedVote.withdraw();
-    });
-
-    it('Emit Withdraw', async() => {
-      await token.transfer(user1, 100);
-      await token.approve(timedVote.address, 100, {from: user1});
-      await timedVote.commit(100);
-      const { logs: events } = await timedVote.withdraw();
-      assert.isAtLeast(events.length, 1);
-      const event = events.pop();
-      assert.strictEqual(event.event, 'Withdraw');
-      assert.strictEqual(event.args.account, user1);
-      assert.isTrue(BigNumber(event.args.value).isEqualTo(100));
+      it('Emit Withdraw', async() => {
+        await token.transfer(user1, 100);
+        await token.approve(timedVote.address, 100, {from: user1});
+        await timedVote.commit(100);
+        const { logs: events } = await timedVote.withdraw();
+        assert.isAtLeast(events.length, 1);
+        const event = events.pop();
+        assert.strictEqual(event.event, 'Withdraw');
+        assert.strictEqual(event.args.account, user1);
+        assert.isTrue(BigNumber(event.args.value).isEqualTo(100));
+      });
     });
   });
 });
