72443fc2caa8551754e83f87658ee105128a4f43
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-10-29 21:52:12 -0400

    vote: Extend comment alignment

--- contracts/ownership/TimedVote.sol
+++ contracts/ownership/TimedVote.sol
@@ -23,8 +23,8 @@ contract TimedVote {
 
   // Commitment of MYB tokens to voting
   struct Commitment {
-    uint256 value;                // MYB amount
-    uint256 time;                 // Commit instant
+    uint256 value;                              // MYB amount
+    uint256 time;                               // Commit instant
   }
 
   // -----
