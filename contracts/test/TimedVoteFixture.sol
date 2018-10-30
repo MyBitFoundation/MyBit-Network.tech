d71b5418eaa17dbe55ddd05aaa5d05c9b972d73a
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-10-30 19:27:30 -0400

    vote: Add commitment time traveling to fixture

--- contracts/test/TimedVoteFixture.sol
+++ contracts/test/TimedVoteFixture.sol
@@ -50,6 +50,49 @@ contract TimedVoteFixture is TimedVote {
   // Writer
 
   /**
+   * Advance commitment time by seconds
+   * @param _account - Account owning commitment to advance.
+   * @param _seconds - Seconds to advance commitment.
+   */
+  function _advanceCommitmentSeconds(address _account, uint256 _seconds)
+  public {
+    commitments[_account].time = commitments[_account].time.sub(_seconds);
+  }
+
+  /**
+   * Advance commitment time by minutes
+   * @param _account - Account owning commitment to advance.
+   * @param _minutes - Minutes to advance commitment.
+   */
+  function _advanceCommitmentMinutes(address _account, uint256 _minutes)
+  public {
+    uint256 _seconds = _minutes.mul(60);
+    _advanceCommitmentSeconds(_account, _seconds);
+  }
+
+  /**
+   * Advance commitment time by hours
+   * @param _account - Account owning commitment to advance.
+   * @param _hours - Hours to advance commitment.
+   */
+  function _advanceCommitmentHours(address _account, uint256 _hours)
+  public {
+    uint256 _minutes = _hours.mul(60);
+    _advanceCommitmentMinutes(_account, _minutes);
+  }
+
+  /**
+   * Advance commitment time by days
+   * @param _account - Account owning commitment to advance.
+   * @param _days - Days to advance commitment.
+   */
+  function _advanceCommitmentDays(address _account, uint256 _days)
+  external {
+    uint256 _hours = _days.mul(24);
+    _advanceCommitmentHours(_account, _hours);
+  }
+
+  /**
    * Set account commitment
    * @param _account - Account to set commitment of.
    * @param _amount - MYB commitment amount.
