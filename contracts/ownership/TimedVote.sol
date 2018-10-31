1883b35f77ff3fb6767068c3af2738ebad38a2ea
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-10-31 04:47:37 -0400

    vote: Define TIER_1_MULTIPLIER constant

--- contracts/ownership/TimedVote.sol
+++ contracts/ownership/TimedVote.sol
@@ -27,6 +27,7 @@ contract TimedVote {
   address constant NULL_ADDRESS = address(0);   // Null address
   uint256 constant TIER_2_AGE = 180 days;       // Tier 2 commitment age
   uint256 constant TIER_3_AGE = 365 days;       // Tier 3 commitment age
+  uint8 constant TIER_1_MULTIPLIER = 100;       // Tier 1 multiplier
   uint8 constant TIER_2_MULTIPLIER = 150;       // Tier 2 multiplier
   uint8 constant TIER_3_MULTIPLIER = 200;       // Tier 3 multiplier
 
