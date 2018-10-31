267a9da2db7745ff02b7284e3c12a30a362d3e73
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-10-31 03:13:53 -0400

    vote: Remove commitment scoped time travel
    Replaced with centralized time travel.

--- contracts/test/TimedVoteFixture.sol
+++ contracts/test/TimedVoteFixture.sol
@@ -120,49 +120,6 @@ contract TimedVoteFixture is TimedVote {
   }
 
   /**
-   * Advance commitment time by seconds
-   * @param _account - Account owning commitment to advance.
-   * @param _seconds - Seconds to advance.
-   */
-  function _advanceCommitmentSeconds(address _account, uint256 _seconds)
-  public {
-    commitments[_account].time = commitments[_account].time.sub(_seconds);
-  }
-
-  /**
-   * Advance commitment time by minutes
-   * @param _account - Account owning commitment to advance.
-   * @param _minutes - Minutes to advance.
-   */
-  function _advanceCommitmentMinutes(address _account, uint256 _minutes)
-  public {
-    uint256 _seconds = _minutes.mul(60);
-    _advanceCommitmentSeconds(_account, _seconds);
-  }
-
-  /**
-   * Advance commitment time by hours
-   * @param _account - Account owning commitment to advance.
-   * @param _hours - Hours to advance.
-   */
-  function _advanceCommitmentHours(address _account, uint256 _hours)
-  public {
-    uint256 _minutes = _hours.mul(60);
-    _advanceCommitmentMinutes(_account, _minutes);
-  }
-
-  /**
-   * Advance commitment time by days
-   * @param _account - Account owning commitment to advance.
-   * @param _days - Days to advance.
-   */
-  function _advanceCommitmentDays(address _account, uint256 _days)
-  external {
-    uint256 _hours = _days.mul(24);
-    _advanceCommitmentHours(_account, _hours);
-  }
-
-  /**
    * Set account commitment
    * @param _account - Account to set commitment of.
    * @param _amount - MYB commitment amount.
