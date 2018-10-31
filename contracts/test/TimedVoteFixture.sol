626354bc7cd052f515903559914d37fe20cc34e8
Book Moons <35854232+bookmoons@users.noreply.github.com>
2018-10-31 02:50:19 -0400

    vote: Add time traveling to fixture

--- contracts/test/TimedVoteFixture.sol
+++ contracts/test/TimedVoteFixture.sol
@@ -215,6 +215,45 @@ contract TimedVoteFixture is TimedVote {
     commitments[_account] = Commitment(_amount, time());
   }
 
+  /**
+   * Advance time by seconds
+   * @param _seconds - Seconds to advance.
+   */
+  function _timeTravelSeconds(uint256 _seconds)
+  public {
+    timestamp = timestamp.add(_seconds);
+  }
+
+  /**
+   * Advance time by minutes
+   * @param _minutes - Minutes to advance.
+   */
+  function _timeTravelMinutes(uint256 _minutes)
+  public {
+    uint256 _seconds = _minutes.mul(60);
+    _timeTravelSeconds(_seconds);
+  }
+
+  /**
+   * Advance time by hours
+   * @param _hours - Hours to advance.
+   */
+  function _timeTravelHours(uint256 _hours)
+  public {
+    uint256 _minutes = _hours.mul(60);
+    _timeTravelMinutes(_minutes);
+  }
+
+  /**
+   * Advance time by days
+   * @param _days - Days to advance.
+   */
+  function _timeTravelDays(uint256 _days)
+  external {
+    uint256 _hours = _days.mul(24);
+    _timeTravelHours(_hours);
+  }
+
   // --------
   // Override
   // --------
