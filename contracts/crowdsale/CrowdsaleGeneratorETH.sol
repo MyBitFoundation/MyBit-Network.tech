8c2fb5de61d67828a09dfc1d4418cc9ed14befd5
Iggy Grey <github@pmphillips.ca>
2019-03-08 20:01:30 -0800

    Small edit to pass value along to kyber when using eth

--- contracts/crowdsale/CrowdsaleGeneratorETH.sol
+++ contracts/crowdsale/CrowdsaleGeneratorETH.sol
@@ -42,9 +42,14 @@ contract CrowdsaleGeneratorETH {
   // @param (uint) _assetManagerPerc = The percentage of the total revenue which is to go to the AssetManager if asset is a success
   function createAssetOrderETH(string _assetURI, address _assetManager, bytes32 _operatorID, uint _fundingLength, uint _startTime, uint _amountToRaise, uint _assetManagerPerc, uint _escrow, address _burnToken)
   external
+  payable
   // burnRequired
   returns (bool) {
-    require(burner.burn(msg.sender, database.uintStorage(keccak256(abi.encodePacked(msg.sig, address(this)))), _burnToken));
+    if(_burnToken == address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE)){
+      require(burner.burn.value(msg.value)(msg.sender, database.uintStorage(keccak256(abi.encodePacked(msg.sig, address(this)))), _burnToken));
+    } else {
+      require(burner.burn(msg.sender, database.uintStorage(keccak256(abi.encodePacked(msg.sig, address(this)))), _burnToken));
+    }
     require(msg.sender == _assetManager || database.boolStorage(keccak256(abi.encodePacked("approval", _assetManager, msg.sender, address(this), msg.sig))), "User not approved");
     require(_amountToRaise > 0, "Crowdsale goal is zero");
     require(_assetManagerPerc < 100, "Manager percent need to be less than 100");
