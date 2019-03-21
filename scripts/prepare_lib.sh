9b74be47719c3e005819c6bec821cb830749b3ff
Peter Phillips <github@pmphillips.ca>
2019-03-21 14:29:28 -0700

    Optimizing npm package

--- scripts/prepare_lib.sh
+++ scripts/prepare_lib.sh
@@ -22,10 +22,14 @@ for filename in build/contracts/*.json; do
   filename_base=$(basename $filename .json)
   if [[ ${map["$filename_base"]} ]] ; then
     touch "lib/js/$filename_base.js"
-    echo -e "\"use strict\";\n" > "lib/js/$filename_base.js"
-    echo -e "Object.defineProperty(exports, \"__esModule\", {\n\tvalue: true\n});\n" >> "lib/js/$filename_base.js"
+    #echo -e "\"use strict\";\n" > "lib/js/$filename_base.js"
+    #echo -e "Object.defineProperty(exports, \"__esModule\", {\n\tvalue: true\n});\n" >> "lib/js/$filename_base.js"
     echo -e "var $filename_base = exports.$filename_base = " >> "lib/js/$filename_base.js"
-    cat "build/contracts/$filename_base.json" >> "lib/js/$filename_base.js"
+    echo -e "$(cat lib/js/$filename_base.js){\nabi: " > "lib/js/$filename_base.js"
+    cat "build/contracts/$filename_base.json" | jq '.abi' >> "lib/js/$filename_base.js"
+    echo -e "$(cat lib/js/$filename_base.js),\nbytecode: " > "lib/js/$filename_base.js"
+    cat "build/contracts/$filename_base.json" | jq '.bytecode' >> "lib/js/$filename_base.js"
+    echo -e "}" >> "lib/js/$filename_base.js"
     echo -e "var _$filename_base = require(\"./js/$filename_base\");\n" >> "lib/index.js"
     echo -e "Object.defineProperty(exports, \"$filename_base\", {\n\tenumerable: true,\n\tget: function get() {\n\t\treturn _$filename_base.$filename_base;\n\t}\n});\n" >> "lib/index.js"
     echo -e "Generated $filename_base.json into $filename_base.js"
